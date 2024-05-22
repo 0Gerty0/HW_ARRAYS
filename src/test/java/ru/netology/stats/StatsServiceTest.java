aeda3d7ff330c191316b3c8a5f245167ca0c8bc6
Aleks Danilushkin <thenumber@ya.ru>
2024-05-22 22:33:59 +0300

    Initial commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,63 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+    @Test
+    public void testCommonSales() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.commonSales(sales);
+        long expected = 180;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAverageSales() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.averageSale(sales);
+        long expected = 180 / 12;
+    }
+
+    @Test
+    void testMaximumSalesMonth() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.maximumSalesMonth(sales);
+        long expected = 8;
+    }
+
+    @Test
+    void testMinimumSalesMonth() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.minimumSalesMonth(sales);
+        long expected = 9;
+    }
+
+    @Test
+    void testMonthBellowAverage() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.calcNumberMonthBellowAverage(sales);
+        long expected = 5;
+    }
+
+    @Test
+    void testMonthHigherAverage() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.minimumSalesMonth(sales);
+        long expected = 5;
+    }
+}
